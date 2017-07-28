c966f6ba3684884d4deb9aa3624373b439b638d9
golkedj <golkedj@gmail.com>
2017-07-28 13:48:05 -0500

    Section 5, Lecture 23

---
+++ Variables/variables.py
@@ -0,0 +1,31 @@
+# greeting = "Bruce"
+# _myName = "David"
+# Dave45 = "Good"
+# Dave_Was_57 = "Hello"
+# Greeting = "There"
+# print(Dave_Was_57 + ' ' + greeting)
+#
+# age = 24
+# print(age)
+#
+# print(greeting + str(age))
+
+# a = 12
+# b = 3
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a // b)
+# print(a % b)
+#
+# for i in range(1, a//b):
+#     print(i)
+
+a = 12
+b = 3
+print(a + b / 3 - 4 * 12)
+print(8 / 2 * 3)
+print(8 * 3 / 2)
+
+print((((a + b) / 3) - 4) * 12)
