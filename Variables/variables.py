831411da951b7a79152405ecf6d019e407d785b9
golkedj <golkedj@gmail.com>
2017-07-28 13:58:05 -0500

    Section 5, Lecture 24

--- Variables/variables.py
+++ Variables/variables.py
@@ -1,31 +1,41 @@
-# greeting = "Bruce"
-# _myName = "David"
-# Dave45 = "Good"
-# Dave_Was_57 = "Hello"
-# Greeting = "There"
-# print(Dave_Was_57 + ' ' + greeting)
-#
-# age = 24
-# print(age)
-#
-# print(greeting + str(age))
-
 # a = 12
 # b = 3
-# print(a + b)
-# print(a - b)
-# print(a * b)
-# print(a / b)
-# print(a // b)
-# print(a % b)
+# print(a + b / 3 - 4 * 12)
+# print(8 / 2 * 3)
+# print(8 * 3 / 2)
 #
-# for i in range(1, a//b):
-#     print(i)
+# print((((a + b) / 3) - 4) * 12)
+#
+# c = a + b
+# d = c / 3
+# e = d - 4
+# print(e * 12)
 
-a = 12
-b = 3
-print(a + b / 3 - 4 * 12)
-print(8 / 2 * 3)
-print(8 * 3 / 2)
+parrot = "Norwegian Blue"
+print(parrot)
+print(parrot[0])
+print(parrot[3])
+print(parrot[-1])
+print(parrot[0:6])
+print(parrot[:6])
+print(parrot[6:])
+print(parrot[-4:-2])
+print(parrot[0:6:2])
+print(parrot[0:6:3])
 
-print((((a + b) / 3) - 4) * 12)
+number = "9,223,372,036,854,775,807"
+print(number[1::4])
+numbers = "1, 2, 3, 4, 5, 6, 7, 8, 9"
+print(numbers[0::3])
+string1 = "he's "
+string2 = "probably "
+print(string1 + string2)
+print("he's " "probably " "pining")
+print("Hello " * 5)
+print("Hello " * (5 + 4))
+print("Hello " * 5 + "4")
+today = "friday"
+print("day" in today)
+print("fri" in today)
+print("thur" in today)
+print("parrot" in "fjord")
