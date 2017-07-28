df9ddd3fb29909cf0dd3e42a38a005a17f062d9b
golkedj <golkedj@gmail.com>
2017-07-28 14:17:31 -0500

    Section 5, Lecture 25

--- StringFormatting/stringformatting.py
+++ StringFormatting/stringformatting.py
@@ -32,7 +32,8 @@ for i in range(1, 12):
 
 print("Pi is approximately {0:12.50f}".format( 22 / 7))
 
-print("January: {2}, February: {0}, March: {2}, April: {1}, May: {2}, June: {1}, July: {2}, August: {2}, September: {1}, October: {2}, November: {1}, December: {2}".format(28, 30, 31))
+print("January: {2}, February: {0}, March: {2}, April: {1}, May: {2}, June: {1}, July: {2}, August: {2}, September: {"
+      "1}, October: {2}, November: {1}, December: {2}".format(28, 30, 31))
 
 for i in range(1, 12):
     print("No. {:<2} squared is {:<4} and cubed is {:<4}".format(i, i ** 2, i ** 3))
