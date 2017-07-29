a1ede3cd44680bbec97aee81000a9480946998d8
golkedj <golkedj@gmail.com>
2017-07-29 10:13:24 -0500

    Section 7, Lecture 45

--- Ranges/ranges.py
+++ Ranges/ranges.py
@@ -1,47 +1,44 @@
-# my_list = list(range(10))
-# print(my_list)
+# decimals = range(0, 100)
+# my_range = decimals[3:40:3]
+# print(my_range == range(3, 40, 3))
+# print(range(0, 5, 2) == range(0, 6, 2))
+# print(list(range(0, 5, 2)))
+# print(list(range(0, 6, 2)))
 #
-# even = list(range(0, 1000, 2))
-# odd = list(range(1, 1000, 2))
+# r = range(0, 100)
+# print(r)
 #
-# print(even)
-# print(odd)
-# my_string = 'abcdefghijklmnopqrstuvwxyz'
-# print(my_string.index('e'))
-# print(my_string[4])
+# for i in r[::-2]:
+#     print(i)
 #
-# small_decimals = range(0, 10)
-# print(small_decimals)
+# print('=' * 50)
+# for i in range(99, 0, -2):
+#     print(i)
 #
-# print(small_decimals.index(3))
+# print('=' * 50)
+# print(range(0, 100)[::-2] == range(99, 0, -2))
 #
-# odd = range(1, 10000, 2)
-# print(odd)
-#
-# print(odd[985])
-#
-# sevens = range(7, 1000000, 7)
-# x = int(input("Please enter a positive number less than one million: "))
-# if x in sevens:
-#     print("{} is divisible by seven".format(x))
-#
-# print(small_decimals)
-#
-# my_range = small_decimals[::2]
-# print(my_range)
-# print(my_range.index(4))
-decimals = range(0, 100)
-print(decimals)
+# for i in range(0, 100, -2):
+#     print(i)
 
-my_range = decimals[3:40:3]
-print(my_range)
+backString = "egaugnal lufrewop yrev a si nohtyP"
+print(backString[::-1])
 
-for i in my_range:
+r = range(0, 10)
+for i in r[::-1]:
     print(i)
 
-print('=' * 40)
-
-for i in range(3, 40, 3):
+# Experiment with different ranges and slices to get a feel for how they work.
+# Remember that you can print the range as well as iterating through it to print
+# its values, to check that your ranges are what you expected.
+# You may also want to include things like.
+#
+o = range(0, 100, 4)
+print(o)
+p = o[::5]
+print(p)
+for i in p:
     print(i)
 
-print(my_range == range(3, 40, 3))
+# and see if you can work out what will be printed before running the program. If you are unsure, use a
+# for loop to print out the values of o to see why p returns what it does.
