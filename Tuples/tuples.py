a3a6fdfefde311aea55fa01ce4c4aa35ff557e6c
golkedj <golkedj@gmail.com>
2017-07-30 16:40:30 -0500

    Section 7, Lecture 47

--- Tuples/tuples.py
+++ Tuples/tuples.py
@@ -1,18 +1,34 @@
-# t = 'a', 'b', 'C'
-# print(t)
+# Given the tuple below that represents the Imelda May album "More Mayhem", write
+# code to print the album details, followed by a listing of all the tracks in the album.
 #
-# print('a', 'b', 'c')
-# print(('a', 'b', 'c'))
-welcome = "welcome to my Nightmare", "Alice Cooper", 1975
-bad = "Bad Company", "Bad Company", 1974
-budgie = "Nightflight", "Budgie", 1981
-imelda = "More Mayhem", "Imelda May", 2011
-metallica = "Ride the Lightning", "Metallica", 1984
+# Indent the tracks by a single tab stop when printing them (remember that you can pass
+# more than one item to the print function, separating them with a comma).
 
-metallica2 = ["Ride the Lightning", "Metallica", 1984]
-print(metallica2)
+# imelda = "More Mayhem", "Imelda May", 2011, (
+#     (1, "Pulling the Rug"), (2, "Psycho"), (3, "Mayhem"), (4, "Kentish Town Waltz"))
+#
+# print(imelda)
+#
+# title, artist, year, tracks = imelda
+# print(title)
+# print(artist)
+# print(year)
+# for song in tracks:
+#     track, title = song
+#     print("\tTrack number {}, Title {}".format(track, title))
 
-title, artist, year = imelda
+imelda = "More Mayhem", "Imelda May", 2011, (
+    [(1, "Pulling the Rug"), (2, "Psycho"), (3, "Mayhem"), (4, "Kentish Town Waltz")])
+
+print(imelda)
+
+imelda[3].append((5, "All For You"))
+
+title, artist, year, tracks = imelda
+tracks.append((6, "Eternity"))
 print(title)
 print(artist)
 print(year)
+for song in tracks:
+    track, title = song
+    print("\tTrack number {}, Title: {}".format(track, title))
