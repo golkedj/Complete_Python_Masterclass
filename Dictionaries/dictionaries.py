fe6be75f3549e5ad7aba81c3c53cea846e0beea5
golkedj <golkedj@gmail.com>
2017-08-05 10:45:53 -0500

    Section 9, Lecture 54

--- Dictionaries/dictionaries.py
+++ Dictionaries/dictionaries.py
@@ -4,22 +4,45 @@ fruit = {"orange": "a sweet, orange, citrus fruit",
          "grape": "a small, sweet fruit growing in bunches",
          "lime": "a sour, green citrus fruit"}
 
-# print(fruit)
-# print(fruit["lemon"])
-# fruit["pear"] = "an odd shaped apple"
-# print(fruit)
-# fruit["lime"] = "great with tequila"
-# print(fruit)
-# #del fruit["lemon"]
-# #fruit.clear()
-# #print(fruit)
-#print(fruit["tomato"])
-while True:
-    dict_key = input("Please enter a fruit: ")
-    if dict_key == "quit":
-        break
-    if dict_key in fruit:
-        description = fruit.get(dict_key)
-        print(description)
-    else:
-        print("we don't have a " + dict_key)
+print(fruit)
+# while True:
+#     dict_key = input("Please enter a fruit: ")
+#     if dict_key == "quit":
+#         break
+#     # description = fruit.get(dict_key, "We don't have a " + dict_key)
+#     # print(description)
+#     if dict_key in fruit:
+#         description = fruit.get(dict_key)
+#         print(description)
+#     else:
+#         print("we don't have a " + dict_key)
+
+# for snack in fruit:
+#     print(fruit[snack])
+
+# for i in range(10):
+#     for snack in fruit:
+#         print(snack + " is " + fruit[snack])
+#     print('-' * 40)
+
+# ordered_keys = list(fruit.keys())
+# ordered_keys.sort()
+# ordered_keys = sorted(list(fruit.keys()))
+# for f in ordered_keys:
+#     print(f + " - " + fruit[f])
+
+# for f in sorted((fruit.keys())):
+#     print(f + " - " + fruit[f])
+# for val in fruit.values():
+#     print(val)
+#
+# print('-' * 40)
+#
+# for key in fruit:
+#     print(fruit[key])
+
+fruit_keys = fruit.keys()
+print(fruit_keys)
+
+fruit["tomato"] = "not nice with ice cream"
+print(fruit_keys)
