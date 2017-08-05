ac7ec31c724388b75dd6ede9ce7717dc1ca5200d
golkedj <golkedj@gmail.com>
2017-08-05 10:32:41 -0500

    Section 9, Lecture 53

---
+++ Dictionaries/dictionaries.py
@@ -0,0 +1,25 @@
+fruit = {"orange": "a sweet, orange, citrus fruit",
+         "apple": "good for making cider",
+         "lemon": "a sour, yellow citrus fruit",
+         "grape": "a small, sweet fruit growing in bunches",
+         "lime": "a sour, green citrus fruit"}
+
+# print(fruit)
+# print(fruit["lemon"])
+# fruit["pear"] = "an odd shaped apple"
+# print(fruit)
+# fruit["lime"] = "great with tequila"
+# print(fruit)
+# #del fruit["lemon"]
+# #fruit.clear()
+# #print(fruit)
+#print(fruit["tomato"])
+while True:
+    dict_key = input("Please enter a fruit: ")
+    if dict_key == "quit":
+        break
+    if dict_key in fruit:
+        description = fruit.get(dict_key)
+        print(description)
+    else:
+        print("we don't have a " + dict_key)
