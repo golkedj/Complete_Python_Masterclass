6a32cf9f21954470d65a065d8f88eecb5821a227
golkedj <golkedj@gmail.com>
2017-08-27 15:32:04 -0500

    Lecture: Writing Binary Files Manually

--- Binary/binary.py
+++ Binary/binary.py
@@ -1,2 +1,30 @@
-for i in range(17):
-    print("{0:>2} in binary is {0:>08b}".format(i))
+# with open("binary", 'bw') as bin_file:
+#     bin_file.write(bytes(range(17)))
+#
+# with open("binary", 'br') as binFile:
+#     for b in binFile:
+#         print(b)
+
+a = 65534       # 00 00 FF FE
+b = 65535       # 00 00 FF FF
+c = 65536       # 00 01 00 00
+x = 2998302     # 02 2D C0 1E
+
+with open("binary2", 'bw') as bin_file:
+    bin_file.write(a.to_bytes(2, 'big'))
+    bin_file.write(b.to_bytes(2, 'big'))
+    bin_file.write(c.to_bytes(4, 'big'))
+    bin_file.write(x.to_bytes(4, 'big'))
+    bin_file.write(x.to_bytes(4, 'little'))
+
+with open("binary2", 'br') as bin_file:
+    e = int.from_bytes(bin_file.read(2), 'big')
+    print(e)
+    f = int.from_bytes(bin_file.read(2), 'big')
+    print(f)
+    g = int.from_bytes(bin_file.read(4), 'big')
+    print(g)
+    h = int.from_bytes(bin_file.read(4), 'big')
+    print(h)
+    i = int.from_bytes(bin_file.read(4), 'big')
+    print(i)
