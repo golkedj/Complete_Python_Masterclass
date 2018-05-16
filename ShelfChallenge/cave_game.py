78644840c2c7a6ec58b40ef166ee6a14e64866a6
golkedj <golkedj@gmail.com>
2018-05-15 18:47:49 -0600

    Lecture: Challenge Continued

--- ShelfChallenge/cave_game.py
+++ ShelfChallenge/cave_game.py
@@ -1,16 +1,51 @@
 import shelve
 
 # My Solution
+# locations = shelve.open('locations')
+# vocabulary = shelve.open('vocabulary')
+#
+# loc = "1"
+# while True:
+#     availableExits = ", ".join(locations[loc]["exits"].keys())
+#
+#     print(locations[loc]["desc"])
+#
+#     if loc == "0":
+#         break
+#     else:
+#         allExits = locations[loc]["exits"].copy()
+#         allExits.update(locations[loc]["namedExits"])
+#
+#     direction = input("Available exits are " + availableExits + " ").upper()
+#     print()
+#
+#     # Parse the user input, using our vocabulary dictionary if necessary
+#     if len(direction) > 1:  # more than 1 letter, so check vocab
+#         words = direction.split()
+#         for word in words:
+#             if word in vocabulary:   # does it contain a word we know?
+#                 direction = vocabulary[word]
+#                 break
+#
+#     if direction in allExits:
+#         loc = str(allExits[direction])
+#     else:
+#         print("You cannot go in that direction")
+#
+# locations.close()
+# vocabulary.close()
+
+# Professor Solution
 locations = shelve.open('locations')
 vocabulary = shelve.open('vocabulary')
 
-loc = "1"
+loc = '1'
 while True:
     availableExits = ", ".join(locations[loc]["exits"].keys())
 
     print(locations[loc]["desc"])
 
-    if loc == "0":
+    if loc == '0':
         break
     else:
         allExits = locations[loc]["exits"].copy()
@@ -28,9 +63,9 @@ while True:
                 break
 
     if direction in allExits:
-        loc = str(allExits[direction])
+        loc = allExits[direction]
     else:
         print("You cannot go in that direction")
 
-locations.close()
 vocabulary.close()
+locations.close()
