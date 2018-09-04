d473491848e94986518630be513b682ea7b42e31
golkedj <golkedj@gmail.com>
2018-09-03 18:48:33 -0600

    Lecture: Functions Part 2

--- functions/functions.py
+++ functions/functions.py
@@ -5,10 +5,13 @@ def python_food():
     print(" " * left_margin, text)
 
 
-def center_text(text):
-    text = str(text)
+def center_text(*args, sep=' ', end='\n', file=None, flush=False):
+    text = ""
+    for arg in args:
+        text += str(arg) + sep
+    text = text[:-1]
     left_margin = (80 - len(text)) // 2
-    print(" " * left_margin, text)
+    print(" " * left_margin, text, end=end, file=file, flush=flush)
 
 
 # call the function
@@ -17,4 +20,5 @@ center_text("spam, spam and eggs")
 center_text(12)
 center_text("spam, spam, spam and spam")
 
-print("first", "second", 3, 4, "spam")
+center_text("first", "second", 3, 4, "spam", sep=":")
+print()
