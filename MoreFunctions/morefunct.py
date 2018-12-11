8300e0f5196a43399838407d8cdd5e5447def03b
David Golke <golkedj@gmail.com>
2018-12-10 19:46:53 -0700

    Lecture: Fix Function and Draw Circles

--- MoreFunctions/morefunct.py
+++ MoreFunctions/morefunct.py
@@ -1,44 +1,59 @@
+import math
 try:
     import tkinter
 except ImportError: # python 2
     import Tkinter as tkinter
 
 
-def porabola(x):
-    y = x * x / 100
-    return y
+def parabola(page, size):
+    for x in range(size):
+        y = x * x / size
+        plot(page, x, y)
+        plot(page, -x, y)
+
+
+def circle(page, radius, g, h):
+    for x in range(g, g + radius):
+        y = h  + (math.sqrt(radius ** 2 - ((x-g) ** 2)))
+        plot(page, x, y)
+        plot(page, x, 2 * h - y)
+        plot(page, 2 * g - x, y)
+        plot(page, 2 * g - x, 2 * h - y)
 
 
 def draw_axes(page):
     page.update()
-    x_origin = canvas.winfo_width() / 2
-    y_origin = canvas.winfo_height() / 2
+    x_origin = page.winfo_width() / 2
+    y_origin = page.winfo_height() / 2
     page.configure(scrollregion=(-x_origin, -y_origin, x_origin, y_origin))
     page.create_line(-x_origin, 0, x_origin, 0, fill="black")
     page.create_line(0, y_origin, 0, -y_origin, fill="black")
     print(locals())
 
 
-def plot(canvas, x, y):
-    canvas.create_line(x, y, x + 1, y + 1, fill="red")
+def plot(page, x, y):
+    page.create_line(x, -y, x + 1, -y + 1, fill="red")
 
 mainWindow = tkinter.Tk()
 
-mainWindow.title("Porabola")
+mainWindow.title("Parabola")
 mainWindow.geometry("640x480")
 
-canvas = tkinter.Canvas(mainWindow, width=320, height=480)
+canvas = tkinter.Canvas(mainWindow, width=640, height=480)
 canvas.grid(row=0, column=0)
 
-canvas2 = tkinter.Canvas(mainWindow, width=320, height=480, background="blue")
-canvas2.grid(row=0, column=1)
-
-print(repr(canvas), repr(canvas2))
 draw_axes(canvas)
-draw_axes(canvas2)
 
-for x in range(-100, 100):
-    y = porabola(x)
-    plot(canvas, x, -y)
+parabola(canvas, 100)
+parabola(canvas, 200)
+circle(canvas, 100, 100, 100)
+circle(canvas, 100, 100, -100)
+circle(canvas, 100, -100, 100)
+circle(canvas, 100, -100, -100)
+circle(canvas, 10, 30, 30)
+circle(canvas, 10, 30, -30)
+circle(canvas, 10, -30, 30)
+circle(canvas, 10, -30, -30)
+circle(canvas, 30, 0, 0)
 
 mainWindow.mainloop()
