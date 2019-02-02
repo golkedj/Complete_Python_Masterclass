ea262362576d9ed31fb650df3cd93c8c8f9655d0
golkedj <golkedj@gmail.com>
2019-02-02 14:50:45 -0700

    Lecture: Deal Cards

--- Blackjack/blackjack.py
+++ Blackjack/blackjack.py
@@ -31,6 +31,24 @@ def load_images(card_images):
             image = tkinter.PhotoImage(file=name)
             card_images.append((card, image,))
 
+
+def deal_card(frame):
+    # pop the next card off of the deck
+    next_card = deck.pop()
+    # add the image to a Label and display the label
+    tkinter.Label(frame, image=next_card[1], relief='raised').pack(side='left')
+    # now return the card's face value
+    return next_card
+
+
+def deal_dealer():
+    deal_card(dealer_card_frame)
+
+
+def deal_player():
+    deal_card(player_card_frame)
+
+
 # Set up the screen and frames for the dealer and player
 mainWindow.title("Black Jack")
 mainWindow.geometry("640x480")
@@ -59,10 +77,10 @@ player_card_frame.grid(row=2, column=1, sticky='ew', rowspan=2)
 button_frame = tkinter.Frame(mainWindow)
 button_frame.grid(row=3, column=0, columnspan=3, sticky='w')
 
-dealer_button = tkinter.Button(button_frame, text="Dealer")
+dealer_button = tkinter.Button(button_frame, text="Dealer", command=deal_dealer)
 dealer_button.grid(row=0, column=0)
 
-player_button = tkinter.Button(button_frame, text="Player")
+player_button = tkinter.Button(button_frame, text="Player", command=deal_player)
 player_button.grid(row=0, column=1)
 
 # load cards
