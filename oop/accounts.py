4db78cad79da87b593c104cecd6eb1bbb750439d
golkedj <golkedj@gmail.com>
2019-02-03 13:52:47 -0700

    Lecture: Methods Part 1

---
+++ oop/accounts.py
@@ -0,0 +1,34 @@
+class Account:
+    """ Simple account class with balance """
+
+    def __init__(self, name, balance):
+        self.name = name
+        self.balance = balance
+        print("Account created for " + self.name)
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+            self.show_balance()
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.balance:
+            self.balance -= amount
+        else:
+            print("The amount must be greater than zero and no more than your account balance")
+        self.show_balance()
+
+    def show_balance(self):
+        print("Balance is {}".format(self.balance))
+
+
+if __name__ == '__main__':
+    david = Account("Time", 0)
+    david.show_balance()
+
+    david.deposit(1000)
+    # david.show_balance()
+    david.withdraw(500)
+    # david.show_balance()
+
+    david.withdraw(2000)
