3106ace2a6c082be3d8469d1f49fade8ca7c7a0e
golkedj <golkedj@gmail.com>
2019-02-03 14:19:23 -0700

    Lecture: Non Public and Mangling

--- oop/accounts.py
+++ oop/accounts.py
@@ -1,6 +1,7 @@
 import datetime
 import pytz
 
+
 class Account:
     """ Simple account class with balance """
 
@@ -10,30 +11,31 @@ class Account:
         return pytz.utc.localize(utc_time)
 
     def __init__(self, name, balance):
-        self.name = name
-        self.balance = balance
-        self.transaction_list = []
-        print("Account created for " + self.name)
+        self._name = name
+        self.__balance = balance
+        self._transaction_list = [(Account._current_time(), balance)]
+        print("Account created for " + self._name)
+        self.show_balance()
 
     def deposit(self, amount):
         if amount > 0:
-            self.balance += amount
+            self.__balance += amount
             self.show_balance()
-            self.transaction_list.append((Account._current_time(), amount))
+            self._transaction_list.append((Account._current_time(), amount))
 
     def withdraw(self, amount):
-        if 0 < amount <= self.balance:
-            self.balance -= amount
-            self.transaction_list.append((Account._current_time(), -amount))
+        if 0 < amount <= self.__balance:
+            self.__balance -= amount
+            self._transaction_list.append((Account._current_time(), -amount))
         else:
             print("The amount must be greater than zero and no more than your account balance")
         self.show_balance()
 
     def show_balance(self):
-        print("Balance is {}".format(self.balance))
+        print("Balance is {}".format(self.__balance))
 
     def show_transactions(self):
-        for date, amount in self.transaction_list:
+        for date, amount in self._transaction_list:
             if amount > 0:
                 tran_type = "deposited"
             else:
@@ -54,3 +56,13 @@ if __name__ == '__main__':
     david.withdraw(2000)
 
     david.show_transactions()
+
+    steph = Account("Steph", 800)
+    steph.__balance = 200
+    steph.deposit(100)
+    steph.withdraw(200)
+    steph.show_transactions()
+    steph.show_balance()
+    print(steph.__dict__)
+    steph._Account__balance = 40
+    steph.show_balance()
