61fb08b9cbb06fad1fe548ac305225f2f139e4fc
golkedj <golkedj@gmail.com>
2019-02-04 19:54:39 -0700

    Lecture: Getters and Setters

---
+++ Game/main.py
@@ -0,0 +1,17 @@
+from player import Player
+
+david = Player("David")
+
+print(david.name)
+print(david.lives)
+david.lives -= 1
+print(david)
+
+david.lives -= 1
+print(david)
+
+david.lives -= 1
+print(david)
+
+david.lives -= 1
+print(david)
