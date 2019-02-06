3f8ade06840076a6a6e193173ffe364b679e841f
golkedj <golkedj@gmail.com>
2019-02-05 19:46:05 -0700

    Lecture: Inheritance Challenge

--- Game/enemy.py
+++ Game/enemy.py
@@ -1,34 +1,36 @@
+import random
+
 # class Enemy:
 class Enemy(object):
 
     def __init__(self, name="Enemy", hit_points=0, lives=1):
-        self.name = name
-        self.base_hit_points = hit_points
-        self.current_hit_points = hit_points
-        self.lives = lives
-        self.alive = True
+        self._name = name
+        self._base_hit_points = hit_points
+        self._current_hit_points = hit_points
+        self._lives = lives
+        self._alive = True
 
     def take_damage(self, damage):
-        if not self.alive:
-            print("{} is not alive".format(self.name))
+        if not self._alive:
+            print("{} is dead".format(self._name))
             return
 
-        remaining_points = self.current_hit_points - damage
+        remaining_points = self._current_hit_points - damage
         if remaining_points > 0:
-            self.current_hit_points = remaining_points
-            print("I took {} points damage and have {} left".format(damage, self.current_hit_points))
+            self._current_hit_points = remaining_points
+            print("I took {} points damage and have {} left".format(damage, self._current_hit_points))
         else:
-            self.lives -= 1
-            if self.lives > 0:
-                self.current_hit_points = self.base_hit_points
+            self._lives -= 1
+            if self._lives > 0:
+                self._current_hit_points = self._base_hit_points
 
-                print("I lost a life and have {} lives remaining".format(self.lives))
+                print("I lost a life and have {} lives remaining".format(self._lives))
             else:
-                self.alive = False
-                print("{} died".format(self.name))
+                self._alive = False
+                print("{} died".format(self._name))
 
     def __str__(self):
-        return "Name: {0.name}, Lives: {0.lives}, Hit points: {0.current_hit_points}".format(self)
+        return "Name: {0._name}, Lives: {0._lives}, Hit points: {0._current_hit_points}".format(self)
 
 
 class Troll(Enemy):
@@ -38,10 +40,29 @@ class Troll(Enemy):
         super().__init__(name=name, lives=1, hit_points=23)
 
     def grunt(self):
-        print("Me {0.name}. {0.name} stomp you".format(self))
+        print("Me {0._name}. {0._name} stomp you".format(self))
 
 
 class Vampyre(Enemy):
 
+    def __init__(self, name, hit_points=12):
+        super().__init__(name=name, lives=3, hit_points=hit_points)
+
+    def dodges(self):
+        if random.randint(1, 3) == 3:
+            print("***** {0._name} dodges *****".format(self))
+            return True
+        else:
+            return False
+
+    def take_damage(self, damage):
+        if not self.dodges():
+            super().take_damage(damage=damage)
+
+class VampyreKing(Vampyre):
+
     def __init__(self, name):
-        super().__init__(name=name, lives=3, hit_points=12)
+        super().__init__(name=name, hit_points=140)
+
+    def take_damage(self, damage):
+        super().take_damage(damage // 4)
--- Game/main.py
+++ Game/main.py
@@ -1,32 +1,43 @@
-from enemy import Enemy, Troll, Vampyre
+from enemy import Enemy, Troll, Vampyre, VampyreKing
 
-ugly_troll = Troll("Pug")
-print("Ugly troll - {}".format(ugly_troll))
+super_vamp = VampyreKing("Super Fang")
 
-another_troll = Troll("Ug")
-print("Another troll - {}".format(another_troll))
+while super_vamp._alive:
+    super_vamp.take_damage(20)
+    print(super_vamp)
 
-brother = Troll("Urg")
-print(brother)
+# ugly_troll = Troll("Pug")
+# print("Ugly troll - {}".format(ugly_troll))
 
-ugly_troll.grunt()
-another_troll.grunt()
-brother.grunt()
+# another_troll = Troll("Ug")
+# print("Another troll - {}".format(another_troll))
 
-ugly_troll.take_damage(10)
-ugly_troll.take_damage(10)
-ugly_troll.take_damage(10)
-ugly_troll.take_damage(10)
-ugly_troll.take_damage(10)
+# brother = Troll("Urg")
+# print(brother)
+
+# ugly_troll.grunt()
+# another_troll.grunt()
+# brother.grunt()
+
+# ugly_troll.take_damage(10)
+# ugly_troll.take_damage(10)
+# ugly_troll.take_damage(10)
+# ugly_troll.take_damage(10)
+# ugly_troll.take_damage(10)
 
 
-dracula = Vampyre("Dracula")
+# dracula = Vampyre("Dracula")
 
-dracula.take_damage(13)
-print(dracula)
+# dracula.take_damage(13)
+# print(dracula)
 
-dracula.take_damage(3)
-print(dracula)
+# dracula.take_damage(3)
+# print(dracula)
 
-while dracula.alive:
-    dracula.take_damage(1)
+# # while dracula._alive:
+#     # dracula.take_damage(1)
+#     # print(dracula)
+
+# dracula._lives = 0
+# dracula._current_hit_points = 1
+# print(dracula)
