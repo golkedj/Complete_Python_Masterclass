419e7b3103fcdb61e986b78bbd3db84bdd8161f5
golkedj <golkedj@gmail.com>
2019-02-06 18:06:56 -0700

    Lecture: Polymorphism

--- Game/main.py
+++ Game/main.py
@@ -1,43 +1,7 @@
-from enemy import Enemy, Troll, Vampyre, VampyreKing
+a = 3
+b = "dave"
+c = 1, 2, 3
 
-super_vamp = VampyreKing("Super Fang")
-
-while super_vamp._alive:
-    super_vamp.take_damage(20)
-    print(super_vamp)
-
-# ugly_troll = Troll("Pug")
-# print("Ugly troll - {}".format(ugly_troll))
-
-# another_troll = Troll("Ug")
-# print("Another troll - {}".format(another_troll))
-
-# brother = Troll("Urg")
-# print(brother)
-
-# ugly_troll.grunt()
-# another_troll.grunt()
-# brother.grunt()
-
-# ugly_troll.take_damage(10)
-# ugly_troll.take_damage(10)
-# ugly_troll.take_damage(10)
-# ugly_troll.take_damage(10)
-# ugly_troll.take_damage(10)
-
-
-# dracula = Vampyre("Dracula")
-
-# dracula.take_damage(13)
-# print(dracula)
-
-# dracula.take_damage(3)
-# print(dracula)
-
-# # while dracula._alive:
-#     # dracula.take_damage(1)
-#     # print(dracula)
-
-# dracula._lives = 0
-# dracula._current_hit_points = 1
-# print(dracula)
+print(a)
+print(b)
+print(c)
